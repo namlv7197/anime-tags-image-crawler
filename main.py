48468ccfdd7fc8eee198651b2b109690839ab820
namlv7197 <lv.nam.7197@gmail.com>
2023-04-04 16:48:10 +0700

    update

--- main.py
+++ main.py
@@ -7,38 +7,23 @@ def args_parser():
     
     parser.add_argument("--post_id",
                         type=str,
-                        default=-1,
+                        default=1,
                         help="""parse the specific post id to crawl""")
-
-    parser.add_argument("--detach",
-                        type=bool,
-                        default=True,
-                        help="""This argument means that after the tests have finished running, the browser window will remain open and visible to the user. By default, when a Selenium test script finishes running, the browser window will be closed automatically"""
-                        )
     
-    parser.add_argument("--headless",
-                       type=bool,
-                       default=True,
-                       help="""This argument means that Chrome driver will run in a mode that displays a graphical user interface (GUI). By default, when a Selenium test script finishes running, there will be no visible browser window.""",
-                       )
-    
-    parser.add_argument("--maximize_window",
-                        type=bool,
-                        default=False,
-                        help="""This argument means that Chrome driver will start the Chrome browser with its window maximized, so that it takes up the entire screen. By default, the window will be not maximum"""
-                        )
-
-    parser.add_argument("--executable_path",
+    parser.add_argument("--uri",
                         type=str,
-                        default='chromedriver',
-                        help="""This argument means that the directory contains chromedriver file"""
-                        )
+                        default=None,
+                        help="""parse the specific uri to store""")
     
-    parser.add_argument("--require_login",
-                        type=bool,
-                        default=True,
-                        help="""This argument means that website needs user to login to render its content"""
-                        )
+    parser.add_argument("--db",
+                        type=str,
+                        default=1,
+                        help="""parse the specific database name""")
+    
+    parser.add_argument("--collection",
+                        type=str,
+                        default=1,
+                        help="""parse the specific collection name""")
     
     parser.add_argument("--username",
                         type=str,
--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,34 @@
-from src.crawler import ChromeDriver,ChromeOption
+from src.crawler import ChromeDriver
+from selenium.webdriver.chrome.options import Options
 from pymongo import MongoClient
 
 def prepare(args):
-    chrome_options=ChromeOption(detach=args.detach,
-                                headless=args.headless,
-                                maximize_window=args.maximize_window)
+    chrome_options=Options()
+    chrome_options.add_experimental_option('detach',True)
+    chrome_options.add_argument('headless')
+    chrome_options.add_argument('start-maximized')
 
     chrome_driver=ChromeDriver(chrome_options,
-                               executable_path=args.executable_path)
-    if args.require_login:
-        chrome_driver.load_url('https://anime-pictures.net/login?lang=en')
-        chrome_driver.require_login(username=args.username,password=args.password)
+                               executable_path='driver')
     
-    # client=MongoClient('mongodb://test-api-ai:Hahalolo%402022@10.10.11.100:27017/?authSource=test-api-ai')
-    return chrome_driver#,client
+    chrome_driver.load_url('https://anime-pictures.net/login?lang=en')
+    chrome_driver.require_login(username=args.username,password=args.password)
+
+    client=MongoClient(args.uri)
+    return chrome_driver,client
 
 
 def crawl(args):
-    chrome_driver=prepare(args)
-    if args.post_id==-1:
-        post_id=int(open('id.txt','r').read())
-    else:
-        post_id=int(args.post_id)
+    chrome_driver,client=prepare(args)
+    curr_post_id=list(client[args.db][args.collection].find().sort('post_id',-1).limit(1))
+    if curr_post_id==[]:
+        curr_post_id=1
+    
+    post_id=max(args.post_id,curr_post_id)
     while True:
         output=chrome_driver.anime_picture_crawl_from_post_id(post_id)
-        print(output)
-        # client['test-api-ai']['images'].insert_one(output)
-        post_id+=1
-        with open('id.txt','w') as f:
-            f.write(str(post_id))
-        if post_id>=100000:
-            break
+        
+        client[args.db][args.collection].insert_one(output)
+        
 
 
