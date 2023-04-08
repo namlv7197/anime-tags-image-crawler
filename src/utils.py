1c16796b2803a0f0263997603caee37608b84ea0
namlv7197 <129140931+namlv7197@users.noreply.github.com>
2023-04-08 14:23:50 +0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ def prepare(args):
     chrome_options.add_argument('headless')
     chrome_options.add_argument('start-maximized')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')        
 
     chrome_driver=ChromeDriver(chrome_options,
                                executable_path='driver')
